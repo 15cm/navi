deaaeaf628606084f6889f3a374776ba9aff899f
Sinkerine <github@15cm.net>
2019-12-15 14:03:02 -0800

    Fix column colors and key binding

--- navi.plugin.zsh
+++ navi.plugin.zsh
@@ -1,12 +1,10 @@
-_navi_path=$(dirname $0:A)
-
 _call_navi() {
    local buff="$BUFFER"
    zle kill-whole-line
-   local cmd="$(NAVI_USE_FZF_ALL_INPUTS=true "${_navi_path}/navi" --print <> /dev/tty)"
+   local cmd="$(NAVI_USE_FZF_ALL_INPUTS=true /usr/local/bin/navi --col-colors 235 --print <> /dev/tty)"
    zle -U "${buff}${cmd}"
 }
 
 zle -N _call_navi
 
-bindkey '^g' _call_navi
+bindkey -v '^y' _call_navi
